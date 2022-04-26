c96e2df3b6cc12b88b8d1ba420867b466ebda37c
13986874732 <2879363960@qq.com>
2022-04-26 19:31:08 +0800

    update activate pro

--- OneChapter/homework-createModelMatrix.cpp
+++ OneChapter/homework-createModelMatrix.cpp
@@ -4,9 +4,9 @@
 #include<glfw3.h>
 #include<iostream>
 #include<Eigen/Eigen>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
 #ifdef _DEBUG
 #pragma comment(lib, "glfw3d.lib")
 #else

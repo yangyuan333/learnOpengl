920fd318b7e29931fe0dd5f2e2f0a1cc9c1673b0
13986874732 <347065418@qq.com>
2022-04-28 21:48:52 +0800

    add assimp and chapter3

---
+++ ThreeChapter/assimpTest.cpp
@@ -0,0 +1,80 @@
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+#include <vector>
+#include <Eigen/Eigen>
+
+#include "../OneChapter/shader.h"
+
+#ifdef _DEBUG
+#pragma comment(lib, "assimp-vc142-mtd.lib")
+#else
+#pragma comment(lib, "assimp-vc142-mt.lib")
+#endif
+
+using namespace std;
+
+struct Vertex {
+	Eigen::Vector3f Position;
+	Eigen::Vector3f Normal;
+	Eigen::Vector2f TexCoords;
+};
+
+struct Texture {
+	unsigned int id;
+	std::string type;
+};
+
+class Mesh {
+public:
+    /*  网格数据  */
+    vector<Vertex> vertices;
+    vector<unsigned int> indices;
+    vector<Texture> textures;
+    /*  函数  */
+    Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> textures)
+    {
+        this->vertices = vertices;
+        this->indices = indices;
+        this->textures = textures;
+        setupMesh();
+    }
+    void Draw(Shader shader);
+private:
+    /*  渲染数据  */
+    unsigned int VAO, VBO, EBO;
+    /*  函数  */
+    void setupMesh()
+    {
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+        glGenBuffers(1, &EBO);
+
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+        glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int),
+            &indices[0], GL_STATIC_DRAW);
+
+        // 顶点位置
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+        // 顶点法线
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
+        // 顶点纹理坐标
+        glEnableVertexAttribArray(2);
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords));
+
+        glBindVertexArray(0);
+    }
+};
+
+int main()
+{
+    
+}
